89b215af4fd13b57ac0cbdfa2a593d202a8da0a5
mrfrig <franklin@neosmail.com>
2025-12-04 13:49:04 -0500

    1-4 Add REPL start logic

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,5 @@
+import readline from "readline";
+
 export function cleanInput(input: string): string[] {
   const trimmed = input.trim();
   if (trimmed.length > 0) {
@@ -6,4 +8,22 @@ export function cleanInput(input: string): string[] {
       .map((word) => word.toLowerCase());
   }
   return [];
+}
+
+export function startREPL() {
+  const replInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+  replInterface.prompt();
+  replInterface.on("line", (line) => { 
+    const commands = cleanInput(line);
+    if (commands.length > 0) {
+      const command = commands[0];
+      console.log(`Your command was: ${command}`);
+    }
+    
+    replInterface.prompt();
+  });
 }
